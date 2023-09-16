b0f1b17ab3cf520d1f31664bf31ab273d7f0b681
Avulov Aminjon <131667831+avulovaminjon@users.noreply.github.com>
2023-09-16 09:23:25 +0500

    Update problemFive.js

---
+++ Node.js praktikum/Js/problemFive.js
@@ -0,0 +1,9 @@
+// N5
+function absolutelyTrue(a, b, c, d){
+    return (a > 0 && b > 0 && c > 0 && d > 0) ? "true" : "false";
+}
+let a = 15
+    b = 45
+    c = -56
+    d = 12;
+console.log(absolutelyTrue(a, b, c, d))
