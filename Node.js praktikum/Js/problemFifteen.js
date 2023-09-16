8c51966cdd1382e38ed8eed1f5e2d5c5fd989e55
Avulov Aminjon <131667831+avulovaminjon@users.noreply.github.com>
2023-09-16 20:53:38 +0500

    Update problemFifteen.js

--- Node.js praktikum/Js/problemFifteen.js
+++ Node.js praktikum/Js/problemFifteen.js
@@ -1,10 +1,10 @@
 // N15
 
-function minMax(list){
-    let min = Math.min( ...list ),
-        max = Math.max( ...list );
-    return [min, max];
+function minMax(arr){
+    arr.sort((a,b)=>a-b);
+    arr.reverse();       
+    return arr[1];
 }
 
-let list = [1, 2, 3, 4, 5, 6, 7]
+let list = [1, 2, 3, 4, 5, 6, 7, 8]
 console.log(minMax(list))
