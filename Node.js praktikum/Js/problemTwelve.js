2e0acfd3cd30aea91eedccfcbd351a5c5623b193
Avulov Aminjon <131667831+avulovaminjon@users.noreply.github.com>
2023-09-16 10:31:59 +0500

    Update problemTwelve.js

---
+++ Node.js praktikum/Js/problemTwelve.js
@@ -0,0 +1,12 @@
+// N 12
+function arrayCount(list, n){
+    let elementCount = 0
+    list.forEach(function (element) {
+        (n === element) ? elementCount += 1 : elementCount +=0;
+    });
+    return elementCount;
+}
+
+let list = [4, 8, 4, 6, 44, 4, 78, 6]
+let n = 4;
+console.log(arrayCount(list, n));
