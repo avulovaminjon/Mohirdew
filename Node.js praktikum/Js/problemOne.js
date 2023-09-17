885924667764518b844f002e3ce8b46b318307e3
Avulov Aminjon <131667831+avulovaminjon@users.noreply.github.com>
2023-09-17 09:29:37 +0500

    Update problemOne.js

--- Node.js praktikum/Js/problemOne.js
+++ Node.js praktikum/Js/problemOne.js
@@ -1,9 +1,8 @@
 //  N1
 function cheakNumber(a,b,c){
-    let threeDigitNumber = +`${a}${b}${c}`
-    return (threeDigitNumber <= 0)  ? '0':"The number is greater than 0";
+    return (a <= 0 || b <= 0 || c <=0)  ? '0':"";
 }
 let a = 3
     b = 2
-    c = 3;
+    c = -3;
 console.log(cheakNumber(a,b,c))
